8ae6aca9a7fdbf38f07426dc453f9fc370f4b139
d3mian <146174516+ali56545@users.noreply.github.com>
2024-11-26 10:34:47 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,8 @@ window._config = {
         userPoolId: 'eu-north-1_EFPYt0V1I', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '15b11jv500utq6bhjk7lfq2f7u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
+        SECRET_HASH = '1t5e6lv0fq78lr31726vsp5e22pi36e1lueo32hkie24n6olvvl0';
+        
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
